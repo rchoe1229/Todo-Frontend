81c865522d62cf1dff90647b2844f483db3a5664
rchoe1229 <ryan.choe@outlook.com>
2021-03-12 13:15:23 -0700

    Changed Title

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Title">Check it Off</h1>
+        <h1 className="Title">What To Do</h1>
         <TodoForm addTodo={this.addTodo}/>
         <TodoContainer deleteTodo={this.deleteTodo} todos={this.state.todos}/> 
       </div>
